f0d8fd06c41a624f2e642e6ae9871a0018072e90
simzub <78310955+simzub@users.noreply.github.com>
2023-01-18 08:27:25 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Form from './components/Form';
+import InnerLayout from './components/InnerLayout';
+
+function App() {
+  return (
+    <div>
+      <InnerLayout>
+        <Form />
+      </InnerLayout>
+    </div>
+  );
+}
+
+export default App;
